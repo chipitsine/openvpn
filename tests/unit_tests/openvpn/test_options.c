8f99fe0c80e9bea99fcdb41ea5011bc66f59841b
Ilya Shipitsin <chipitsine@gmail.com>
2017-06-16 22:52:46 +0500

    usage test

---
+++ tests/unit_tests/openvpn/test_options.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+static void
+usage_test(void **state)
+{
+    usage();
+}
+
+int
+main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(usage_test),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
